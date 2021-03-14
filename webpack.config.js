81aff87209050a8b5e095f1ab3472d66d1a01544
JerryWang <jerrywang0731@outlook.com>
2021-03-14 16:50:09 +0800

    source map enable

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const { Extension } = require('typescript');
 
 module.exports = {
+    devtool: "eval-source-map",  // "source-map"
     entry: './src/index.ts',
     module: {
         rules: [         
